e331c6452c7de9b94db6987e92c89d03a7987b50
Malte.Marquarding <Malte.Marquarding@80e5cd9e-5d28-0410-a7fc-3f808f404835>
2007-10-29 00:43:10 +0000

    tagged pyrap_quanta 0.1.0

---
+++ pyrap_quanta/tags/pyrap_quanta-0.1.0/pyrap_quanta/__init__.py
@@ -0,0 +1,4 @@
+from quantity import quantity
+from quantity import quantity, is_quantity
+from _quanta import *
+del Quantity, QuantVec, from_string, from_dict, from_dict_v
---
+++ pyrap_quanta/tags/pyrap_quanta-0.1.0/pyrap_quanta/quantity.py
@@ -0,0 +1,38 @@
+from pyrap_quanta._quanta import Quantity
+from pyrap_quanta._quanta import QuantVec
+from pyrap_quanta._quanta import from_string, from_dict, from_dict_v
+
+def is_quantity(q):
+    return isinstance(q, QuantVec) or isinstance(q, Quantity)
+
+# Quantity returns new Quantities, so we need to insert these
+# functions into Quantity
+def new_get_value(quant, *args):
+    val = QuantVec._get_value(quant, *args)
+    if len(val) == 1:
+	return val[0]
+    else:
+	return val
+QuantVec.get_value = new_get_value
+
+def to_string(quant):
+    return "%s %s" % (str(quant.get_value()), quant.get_unit())
+QuantVec.__str__ = to_string
+Quantity.__str__ = to_string
+
+def quantity(*args):
+    if len(args) == 1:
+        if isinstance(args[0], str):
+            # use copy constructor to create quantity from string
+            return Quantity(from_string(args[0]))
+        elif isinstance(args[0], dict):
+            if hasattr(args[0]["value"], "__len__"):
+                return QuantVec(from_dict_v(args[0]))
+            else:
+                return Quantity(from_dict(args[0]))
+    else:
+        if hasattr(args[0], "__len__"):
+            return QuantVec(*args)
+        else:
+            return Quantity(*args)
+
