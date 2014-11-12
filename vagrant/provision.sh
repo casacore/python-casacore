5557f625af21f42c121dd92457de296c8f76a014
Gijs Molenaar <gijs@pythonic.nl>
2014-11-12 11:10:47 +0200

    fix auto build

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -6,10 +6,9 @@ export DEBIAN_FRONTEND=noninteractive
 cp /vagrant/vagrant/apt.sources.list /etc/apt/sources.list
 
 ## install all requirements
-apt-get install -y software-properties-common
-add-apt-repository ppa:ska-sa/main
+apt-get update
+apt-get install -y software-properties-common python-numpy python-nose \
+                    python-setuptools libboost-python-dev libcfitsio3-dev
 add-apt-repository ppa:ska-sa/beta
 apt-get update
-apt-get upgrade -y
-
-apt-get install -y libcasacore-dev casacore-data python-numpy python-nose python-setuptools libboost-python-dev libcfitsio3-dev
+apt-get install -y libcasacore-dev
