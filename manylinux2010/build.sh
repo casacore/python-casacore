d5274acba0e73292c68bd67e0ea799e4d38a17b0
Tammo Jan Dijkema <T.J.Dijkema@gmail.com>
2020-05-05 22:23:38 +0200

    Add python 3.8 wheel
    Resolves #200

--- manylinux2010/build.sh
+++ manylinux2010/build.sh
@@ -3,7 +3,7 @@
 HERE=`dirname "$0"`
 cd $HERE/..
 
-for i in 27 34 35 36 37; do
+for i in 27 34 35 36 37 38; do
     docker build -f manylinux2010/wheel${i}.docker . -t python-casacore${i}
     docker run -v `pwd`/manylinux2010:/manylinux2010 python-casacore${i} sh -c "cp /output/*.whl /manylinux2010/."
 done
