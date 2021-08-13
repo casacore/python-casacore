a0a95d83f9e7c1c5be32c6012b44b63436e264d9
Tammo Jan Dijkema <dijkema@astron.nl>
2021-08-13 16:00:34 +0200

    Fix empty include directory
    Contributed by @gmloose
    Resolves #223

--- setup.py
+++ setup.py
@@ -223,10 +223,13 @@ def get_extensions():
                 if found_lib:
                     depends = depends + [found_lib]
 
+        library_dirs=[lib for lib in (boost_python_libdir, casa_libdir) if lib]
+        include_dirs=[inc for inc in (boost_python_includedir, casa_includedir) if inc]
+
         extensions.append(Extension(name=name, sources=sources,
                                     depends=depends, libraries=libraries,
-                                    library_dirs=[boost_python_libdir, casa_libdir],
-                                    include_dirs=[boost_python_includedir, casa_includedir],
+                                    library_dirs=library_dirs,
+                                    include_dirs=include_dirs,
                                     # Since casacore 3.0.0 we have to be C++11
                                     extra_compile_args=['-std=c++11']))
     return extensions
