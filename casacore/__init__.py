2e2674cce07e2107811dbf07234c9380e1e608bd
Marcel Loose <loose@astron.nl>
2024-02-16 15:28:17 +0100

    WIP: Add logic to find packaged casacore data

--- casacore/__init__.py
+++ casacore/__init__.py
@@ -1,2 +1,13 @@
-__version__ = "3.5.2"
+__version__ = "3.6.0a1"
 __mincasacoreversion__ = "3.1.1"
+
+import os
+
+# If environment variable `AIPSPATH` is not set, then set it to the directory
+# containing the `.aipsrc` file that is distributed with this package.
+# This `.aipsrc` file uses the environment `CASACORE_DATA`, which should point
+# to the directory containing the casacore data files.
+if "AIPSPATH" not in os.environ:
+    root = os.path.dirname(__file__)
+    os.environ["AIPSPATH"] = root
+    os.environ["CASACORE_DATA"] = os.path.join(root, "data")
